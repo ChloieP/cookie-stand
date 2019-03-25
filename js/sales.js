18dd58ca9cc6531fb8be07b104c7601a22a0aed0
ChloieP <chloieparsons@gmail.com>
2019-03-24 20:12:11 -0700

    Still trying to get cookie code to work for first store

--- js/sales.js
+++ js/sales.js
@@ -14,14 +14,109 @@ var pike = {
   getAvgCookiesHour: function() {
     this.getAvgCookiesHour = Math.floor(Math.round) * (this.avgSale * this.getAvgCustHour);
   },
-  dailySalesProjection: function(){
+  render: function(){
     for (var i = 0; i < hours.length; i++) {
+      this.getAvgCookiesHour();
+      this.getAvgCustHour();
 
       var liEl = document.createElement('li');
       liEl.textContent = hours[i] + pike.getAvgCookiesHour;
-      var h3 = document.createElement('1st and Pike');
-      document.getElementById('pike').appendChild(liEl);
+      var h3 = document.createElement('firstPike');
+      document.getElementById('firstPike').appendChild(liEl);
       h3.appendGrandchild(liEl);
     }
   },
 };
+// var seaTac = {
+//   name: 'SeaTac Airport',
+//   minCust: 3,
+//   maxCust: 24,
+//   avgSale: 1.2,
+//   getAvgCustHour: function(minCust, maxCust) {
+//     console.log();
+//     this.getAvgCustHour = Math.floor(Math.random() * (maxCust - minCust + 1) + minCust);
+//   },
+//   getAvgCookiesHour: function() {
+//     this.getAvgCookiesHour = Math.floor(Math.round) * (this.avgSale * this.getAvgCustHour);
+//   },
+//   dailySalesProjection: function(){
+//     for (var i = 0; i < hours.length; i++) {
+
+//       var liEl = document.createElement('li');
+//       liEl.textContent = hours[i] + seaTac.getAvgCookiesHour;
+//       var h3 = document.createElement('SeaTac Airport');
+//       document.getElementById('SeaTac').appendChild(liEl);
+//       h3.appendGrandchild(liEl);
+//     }
+//   },
+// };
+// var seaCenter = {
+//   name: 'Seattle Center',
+//   minCust: 11,
+//   maxCust: 38,
+//   avgSale: 3.7,
+//   getAvgCustHour: function(minCust, maxCust) {
+//     console.log();
+//     this.getAvgCustHour = Math.floor(Math.random() * (maxCust - minCust + 1) + minCust);
+//   },
+//   getAvgCookiesHour: function() {
+//     this.getAvgCookiesHour = Math.floor(Math.round) * (this.avgSale * this.getAvgCustHour);
+//   },
+//   dailySalesProjection: function(){
+//     for (var i = 0; i < hours.length; i++) {
+
+//       var liEl = document.createElement('li');
+//       liEl.textContent = hours[i] + seaCenter.getAvgCookiesHour;
+//       var h3 = document.createElement('Seattle Center');
+//       document.getElementById('SeaCenter').appendChild(liEl);
+//       h3.appendGrandchild(liEl);
+//     }
+//   },
+// };
+// var capitolHill = {
+//   name: 'Capitol Hill',
+//   minCust: 20,
+//   maxCust: 38,
+//   avgSale: 2.3,
+//   getAvgCustHour: function(minCust, maxCust) {
+//     console.log(20, 38);
+//     this.getAvgCustHour = Math.floor(Math.random() * (maxCust - minCust + 1) + minCust);
+//     console.log(20, 38);
+//   },
+//   getAvgCookiesHour: function() {
+//     this.getAvgCookiesHour = Math.floor(Math.round) * (this.avgSale * this.getAvgCustHour);
+//   },
+//   dailySalesProjection: function(){
+//     for (var i = 0; i < hours.length; i++) {
+
+//       var liEl = document.createElement('li');
+//       liEl.textContent = hours[i] + capitolHill.getAvgCookiesHour;
+//       var h3 = document.createElement('Capitol Hill');
+//       document.getElementById('capitolHill').appendChild(liEl);
+//       h3.appendGrandchild(liEl);
+//     }
+//   },
+// };
+// var alki = {
+//   name: 'Alki',
+//   minCust: 2,
+//   maxCust: 16,
+//   avgSale: 4.6,
+//   getAvgCustHour: function(minCust, maxCust) {
+//     console.log();
+//     this.getAvgCustHour = Math.floor(Math.random() * (maxCust - minCust + 1) + minCust);
+//   },
+//   getAvgCookiesHour: function() {
+//     this.getAvgCookiesHour = Math.floor(Math.round) * (this.avgSale * this.getAvgCustHour);
+//   },
+//   dailySalesProjection: function(){
+//     for (var i = 0; i < hours.length; i++) {
+
+//       var liEl = document.createElement('li');
+//       liEl.textContent = hours[i] + alki.getAvgCookiesHour;
+//       var h3 = document.createElement('Alki');
+//       document.getElementById('Alki').appendChild(liEl);
+//       h3.appendGrandchild(liEl);
+//     }
+//   },
+// };
