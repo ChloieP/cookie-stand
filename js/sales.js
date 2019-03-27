cb76e0815ca61db13e3d19e360b6914954a0f598
ChloieP <chloieparsons@gmail.com>
2019-03-27 12:28:16 -0700

    First two stores are working SMILEY FACE

--- js/sales.js
+++ js/sales.js
@@ -1,60 +1,84 @@
 'use strict';
 var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
+var totalCookies = 0;
 var firstPike = {
   minHourlyCustomers: 23,
   maxHourlyCustomers: 65,
   avgCookiesPerSale: 6.3,
-  totalCookies: 0,
-  estCustomersPerHour: 0,
-  cookiesHourlyPurchased:0,
+  randCustomersPerHour: 0,
+  cookiesPurchasedHourly:0,
   cookieArray: [],
-  randCustomersPerHour: function (){
+  getRandCustomersPerHour: function () {
     this.randCustomersPerHour = Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers +1) + this.minHourlyCustomers);
 
   },
-  cookiesPurchasedHourly: function () {
+  getCookiesPurchasedHourly: function () {
     this.cookiesPurchasedHourly = Math.floor(Math.random() * (this.avgCookiesPerSale * this.randCustomersPerHour));
 
   },
-  cookiesByTheTOD: function() {
-    for (var i = 0; i < this.hoursOpen.length; i++) {
-      this.randCustomersPerHour();
-      this.cookiesPurchasedHourly();
+  render: function() {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      this.getRandCustomersPerHour();
+      this.getCookiesPurchasedHourly();
+      var firstPikeuL = document.getElementById('firstPikeSales');
+      var liEl = document.createElement('li');
+      liEl.textContent = hoursOpen[i] + ': ' + this.cookiesPurchasedHourly + ' cookies';
+      firstPikeuL.appendChild(liEl);
       this.cookieArray.push(this.cookiesPurchasedHourly);
+      totalCookies = this.cookiesPurchasedHourly + totalCookies;
+      if (i===hoursOpen.length - 1){
+        liEl = document.createElement('li');
+        liEl.textContent = 'Total: ' + totalCookies;
+        firstPikeuL.appendChild(liEl);
+      }
     }
   },
 };
 
-var render = function() {
-  var parentElementH3 = document.getElementById('firstPike');
-  var newElementuL = document.createElement('firstPikeSales');
-  parentElementH3.appendChild(newElementuL);
+firstPike.getCookiesPurchasedHourly();
+firstPike.getRandCustomersPerHour();
+firstPike.render();
+
+var seaTac = {
+  minHourlyCustomers: 3,
+  maxHourlyCustomers: 24,
+  avgCookiesPerSale: 1.2,
+  randCustomersPerHour: 0,
+  cookiesPurchasedHourly:0,
+  cookieArray: [],
+  getRandCustomersPerHour: function () {
+    this.randCustomersPerHour = Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers +1) + this.minHourlyCustomers);
+
+  },
+  getCookiesPurchasedHourly: function () {
+    this.cookiesPurchasedHourly = Math.floor(Math.random() * (this.avgCookiesPerSale * this.randCustomersPerHour));
+
+  },
+  render: function() {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      this.getRandCustomersPerHour();
+      this.getCookiesPurchasedHourly();
+      var seaTacuL = document.getElementById('seaTacSales');
+      var liEl = document.createElement('li');
+      liEl.textContent = hoursOpen[i] + ': ' + this.cookiesPurchasedHourly + ' cookies';
+      seaTacuL.appendChild(liEl);
+      this.cookieArray.push(this.cookiesPurchasedHourly);
+      totalCookies = this.cookiesPurchasedHourly + totalCookies;
+      console.log(totalCookies);
+      if (i===hoursOpen.length - 1){
+        liEl = document.createElement('li');
+        liEl.textContent = 'Total: ' + totalCookies;
+        seaTacuL.appendChild(liEl);
+        console.log(totalCookies);
+      }
+    }
+  },
 };
 
-// var seaTac = {
-//   name: 'SeaTac Airport',
-//   minCust: 3,
-//   maxCust: 24,
-//   avgSale: 1.2,
-//   getAvgCustHour: function(minCust, maxCust) {
-//     console.log();
-//     this.getAvgCustHour = Math.floor(Math.random() * (maxCust - minCust + 1) + minCust);
-//   },
-//   getAvgCookiesHour: function() {
-//     this.getAvgCookiesHour = Math.floor(Math.round) * (this.avgSale * this.getAvgCustHour);
-//   },
-//   dailySalesProjection: function(){
-//     for (var i = 0; i < hours.length; i++) {
+seaTac.getCookiesPurchasedHourly();
+seaTac.getRandCustomersPerHour();
+seaTac.render();
 
-//       var liEl = document.createElement('li');
-//       liEl.textContent = hours[i] + seaTac.getAvgCookiesHour;
-//       var h3 = document.createElement('SeaTac Airport');
-//       document.getElementById('SeaTac').appendChild(liEl);
-//       h3.appendGrandchild(liEl);
-//     }
-//   },
-// };
 // var seaCenter = {
 //   name: 'Seattle Center',
 //   minCust: 11,
