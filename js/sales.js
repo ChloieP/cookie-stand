4039a748a13d188d4861c714c63355225c9ab8a1
ChloieP <chloieparsons@gmail.com>
2019-03-29 12:12:44 -0700

    Store List, Cookies Per Hour, and Hours Open in table form  WORKS, TO DO Store and Hourly Totals

--- js/sales.js
+++ js/sales.js
@@ -20,39 +20,36 @@ Store_Data.prototype.getRandCustomersPerHour = function () {
 };
 
 Store_Data.prototype.getCookiesPurchasedHourly = function () {
-  return this.cookiesPurchasedHourly = Math.ceil(Math.random() * (this.avgCookiesPerSale * this.randCustomersPerHour));
-
+  for (var i = 0; i < hoursOpen.length; i++) {
+    this.cookiesPurchasedHourly = Math.ceil(Math.random() * (this.avgCookiesPerSale * this.randCustomersPerHour));
+    this.cookieArray.push(this.cookiesPurchasedHourly);
+  }
+  // console.log(this); WORKS - gives me all the stores and their data here - put into table
 };
-
 
 Store_Data.prototype.render = function () {
-  var theStores = document.getElementById('Store_List');
-  var tdStores = document.createElement('td');
-  tdStores.textContent = this.store;
-  theStores.appendChild(tdStores);
-
+  var storesList = document.getElementById('Store_List');
+  var storesListRow = document.createElement('tr');
+  storesListRow.textContent = this.store;
+  storesList.appendChild(storesListRow);
+  for (var i = 0; i < this.cookieArray.length; i++) {
+    var storesCookieSales = document.createElement('td');
+    storesCookieSales.textContent = this.cookieArray[i];
+    storesListRow.appendChild(storesCookieSales);
+  }
 };
+// console.log(this); WORKS - added all stores and data to the table
 
-var theStores2 = document.getElementById('Store_List');
-var tdStores2 = document.createElement('td');
-tdStores2.textContent = this.cookiesPurchasedHourly;
-theStores2.appendChild(tdStores2);
-
+// HEADER: Hours
 for (var i = 0; i < hoursOpen.length; i++) {
   var hoursList = document.getElementById('Hours_List');
   var thElH = document.createElement('Length');
   thElH.textContent = hoursOpen[i] + ' ';
   hoursList.appendChild(thElH);
 }
-
-for (var i = 0; i < this.cookiesPurchasedHourly.length; i++) {
-  var td = document.createElement('td');
-  td.textContent = this.cookieArray[i];
-  // tr.appendChild(td); Look at HTML structure through Slack lenss
-}
+// console.log(this.name); WORKS - add all hours to table
 
 Store_Data.prototype.totalCookies = function(){
-  this.cookieArray.push(this.cookiesPurchasedHourly);
   this.totalCookies = this.cookiesPurchasedHourly + this.totalCookies;
   for (var i = 0; i === hoursOpen.length; i--) {
     var tFoot = document.getElementById('Hourly_Total');
@@ -68,24 +65,24 @@ var seaCenter = new Store_Data('Seattle Center', 11, 38, 3.7);
 var capitolHill = new Store_Data('Capitol Hill', 20, 38, 2.3);
 var alki = new Store_Data('Alki', 2, 16, 4.6);
 
-firstPike.getCookiesPurchasedHourly();
 firstPike.getRandCustomersPerHour();
+firstPike.getCookiesPurchasedHourly();
 firstPike.render();
 
-seaTac.getCookiesPurchasedHourly();
 seaTac.getRandCustomersPerHour();
+seaTac.getCookiesPurchasedHourly();
 seaTac.render();
 
-seaCenter.getCookiesPurchasedHourly();
 seaCenter.getRandCustomersPerHour();
+seaCenter.getCookiesPurchasedHourly();
 seaCenter.render();
 
-capitolHill.getCookiesPurchasedHourly();
 capitolHill.getRandCustomersPerHour();
+capitolHill.getCookiesPurchasedHourly();
 capitolHill.render();
 
-alki.getCookiesPurchasedHourly();
 alki.getRandCustomersPerHour();
+alki.getCookiesPurchasedHourly();
 alki.render();
 
 
