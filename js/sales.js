70633a83c73835f8908b6fb8162af966865c40e9
ChloieP <chloieparsons@gmail.com>
2019-03-29 14:58:19 -0700

    Data ALL rendered except hourly totals

--- js/sales.js
+++ js/sales.js
@@ -1,6 +1,6 @@
 'use strict';
+
 var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-// var allStores = [];
 
 var Store_Data = function(storeName, minHourlyCustomers, maxHourlyCustomers, avgCookiesPerSale) {
   this.store = storeName;
@@ -23,47 +23,65 @@ Store_Data.prototype.getCookiesPurchasedHourly = function () {
   for (var i = 0; i < hoursOpen.length; i++) {
     this.cookiesPurchasedHourly = Math.ceil(Math.random() * (this.avgCookiesPerSale * this.randCustomersPerHour));
     this.cookieArray.push(this.cookiesPurchasedHourly);
+    this.totalCookies += this.cookiesPurchasedHourly;
   }
-  // console.log(this); WORKS - gives me all the stores and their data here - put into table
+  console.log(this);
 };
 
 Store_Data.prototype.render = function () {
-  var storesList = document.getElementById('Store_List');
-  var storesListRow = document.createElement('tr');
-  storesListRow.textContent = this.store;
-  storesList.appendChild(storesListRow);
+  var storesData = document.getElementById('tableBody');
+  var storesDataTr = document.createElement('tr');
+  storesData.appendChild(storesDataTr);
+
+  var storesDataTh = document.createElement('th');
+  storesDataTh.textContent = this.store;
+  storesDataTr.appendChild(storesDataTh);
+
   for (var i = 0; i < this.cookieArray.length; i++) {
     var storesCookieSales = document.createElement('td');
     storesCookieSales.textContent = this.cookieArray[i];
-    storesListRow.appendChild(storesCookieSales);
+    storesDataTr.appendChild(storesCookieSales);
   }
-};
-// console.log(this); WORKS - added all stores and data to the table
+
+  var storesTotals = document.createElement('td');
+  storesTotals.textContent = this.totalCookies;
+  storesDataTr.appendChild(storesTotals);
 
 // HEADER: Hours
-for (var i = 0; i < hoursOpen.length; i++) {
-  var hoursList = document.getElementById('Hours_List');
-  var thElH = document.createElement('Length');
-  thElH.textContent = hoursOpen[i] + ' ';
-  hoursList.appendChild(thElH);
-}
-// console.log(this.name); WORKS - add all hours to table
+var hoursThead = document.getElementById('Hours_Open');
+var hoursTr = document.createElement('tr');
+hoursThead.appendChild(hoursTr);
+var thElH = document.createElement('th');
+hoursTr.appendChild(thElH);
 
-Store_Data.prototype.totalCookies = function(){
-  this.totalCookies = this.cookiesPurchasedHourly + this.totalCookies;
-  for (var i = 0; i === hoursOpen.length; i--) {
-    var tFoot = document.getElementById('Hourly_Total');
-    var trFoot = document.getElementById('All_Stores');
-    trFoot.textContent = `${this.totalCookies}`;
-    tFoot.appendChild(trFoot);
-  }
+for (var i = 0; i < hoursOpen.length; i++) {
+  var thElH = document.createElement('th');
+  thElH.textContent = hoursOpen[i];
+  hoursTr.appendChild(thElH);
+}
+var tHead = document.createElement('th');
+tHead.textContent = 'Totals';
+hoursTr.appendChild(tHead);
+
+// FOOTER: Totals
+Store_Data.prototype.totalCookies = function() {
+  for (var i = 0; i < hoursOpen.length; i++) {
+  //   var sum = this.cookieArray.reduce((a, b) => a + b);
+  //   this.totalCookies.push(sum);
+  // var tFoot = document.getElementById('Hourly_Total');
+  // var trFoot = document.createElement('tr');
+  trFoot.textContent = this.totalCookies;
+  tFoot.appendChild(trFoot);
+  // console.log(firstPike);
 };
+
 
 var firstPike = new Store_Data('First & Pike', 23, 65, 6.3);
 var seaTac = new Store_Data('SeaTac Airport', 3, 24, 1.2);
 var seaCenter = new Store_Data('Seattle Center', 11, 38, 3.7);
 var capitolHill = new Store_Data('Capitol Hill', 20, 38, 2.3);
 var alki = new Store_Data('Alki', 2, 16, 4.6);
+
 
 firstPike.getRandCustomersPerHour();
 firstPike.getCookiesPurchasedHourly();
@@ -84,5 +102,3 @@ capitolHill.render();
 alki.getRandCustomersPerHour();
 alki.getCookiesPurchasedHourly();
 alki.render();
-
-
