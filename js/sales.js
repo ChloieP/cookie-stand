c1246880c75b3d1fcc9c41519ff0f624342f0953
ChloieP <chloieparsons@gmail.com>
2019-04-02 12:59:08 -0700

    Flow is fixed, WOHOOOO, thank you Calvin

--- js/sales.js
+++ js/sales.js
@@ -1,11 +1,14 @@
 'use strict';
+
+// Lines 6 - 102 are the factory and its production; lines 103 & onward are when the product ships
+// Definitions: Global
+
 var stores = [];
 
 var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 function Store_Data(storeName, minHourlyCustomers, maxHourlyCustomers, avgCookiesPerSale) {
   this.store = storeName;
-  stores.push(this);
   this.minHourlyCustomers = minHourlyCustomers;
   this.maxHourlyCustomers = maxHourlyCustomers;
   this.avgCookiesPerSale = avgCookiesPerSale;
@@ -13,7 +16,10 @@ function Store_Data(storeName, minHourlyCustomers, maxHourlyCustomers, avgCookie
   this.cookiesPurchasedHourly = 0;
   this.cookiesHourlyArray = [];
   this.totalCookies = 0;
+  stores.push(this);
 }
+
+//Functions performed on the definitions: Cab Do
 
 Store_Data.prototype.getRandCustomersPerHour = function () {
   this.randCustomersPerHour = Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers);
@@ -50,6 +56,7 @@ Store_Data.prototype.render = function () {
 
 }; // console.log(this); WORKS - added all stores and data to the table
 
+
 // HEADER: Hours
 var hoursThead = document.getElementById('Hours_Open');
 var hoursTr = document.createElement('tr');
@@ -76,6 +83,8 @@ for (var i = 0; i < hoursOpen.length; i++) {
   var hourlyTotal = 0;
   for (var j = 0; j < stores.length; j++); {
     // hourlyTotal += stores[j].cookiesHourlyArray[i];
+    console.log(stores);
+    // console.log('hi munchkin');
   }
 }
 // console.log(stores);
@@ -91,11 +100,13 @@ var FootTd = document.createElement('td');
 FootTd.textContent = hourlyTotal;
 FootTr.appendChild(FootTd);
 
-var firstPike = new Store_Data('First & Pike', 23, 65, 6.3);
+var firstPike = new Store_Data('First & Pike', 23, 65, 6.3); // I am creating a store called First & Pike and as part of that I am also telling it to put itself in an array: stores
 var seaTac = new Store_Data('SeaTac Airport', 3, 24, 1.2);
 var seaCenter = new Store_Data('Seattle Center', 11, 38, 3.7);
 var capitolHill = new Store_Data('Capitol Hill', 20, 38, 2.3);
 var alki = new Store_Data('Alki', 2, 16, 4.6);
+
+console.log(stores);
 
 firstPike.getRandCustomersPerHour();
 firstPike.getCookiesPurchasedHourly();
